659c8a550530f2325c7e570f0373e3cd3535f548
LinardsS <linards.solovjevs@gmail.com>
2021-10-26 23:49:38 +0300

    Initial role setup

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+use App\Models\Role;
+use DB;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::truncate();
+        DB::table('role_user')->truncate();
+
+        $adminRole = Role::where('name', 'admin')->first();
+        $userRole = Role::where('name', 'user')->first();
+        $ownerRole = Role::where('name', 'owner')->first();
+
+        $admin = User::create([
+          'name' => 'Admin User',
+          'email' => 'admin@test.com',
+          'password' => Hash::make('1234')
+        ]);
+        $user = User::create([
+          'name' => 'Generic User',
+          'email' => 'user@test.com',
+          'password' => Hash::make('1234')
+        ]);
+        $owner = User::create([
+          'name' => 'Owner User',
+          'email' => 'owner@test.com',
+          'password' => Hash::make('1234')
+        ]);
+
+        $admin->roles()->attach($adminRole);
+        $user->roles()->attach($userRole);
+        $owner->roles()->attach($ownerRole);
+    }
+}
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,5 +25,6 @@
     <footer id="footer" class="text-center">
       <p>Copyright 2021 &copy; Prognozis</p>
     </footer>
+     <script src="/docs/5.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
 </html>
