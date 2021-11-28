8a9b1d235a7338b2e559bde59115fa6c7db554e9
LinardsS <linards.solovjevs@gmail.com>
2021-11-28 20:03:35 +0200

    Function that updates NHL game results for specified time period

--- app/Http/Controllers/ResultsController.php
+++ app/Http/Controllers/ResultsController.php
@@ -6,6 +6,7 @@ use App\Models\Result;
 use App\Models\Game;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
+use DateTime;
 
 class ResultsController extends Controller
 {
@@ -97,5 +98,47 @@ class ResultsController extends Controller
       $authKey = "5dd4dc55b02c40888d7fba33f492599b";
       $response = Http::withHeaders(['X-Auth-Token' => $authKey])->get('http://api.football-data.org/v2/competitions/PL/matches/?matchday' . $matchDay);
       return $response;
+    }
+
+    public function getNHLResults($startDate)
+    {
+      $endDate = new DateTime();
+      $endDate = $endDate->format('Y-m-d');
+      $request = Http::get('http://statsapi.web.nhl.com/api/v1/schedule?startDate=' . $startDate . '&endDate=' . $endDate);
+      $dates = json_decode($request)->dates;
+      $output = [];
+      foreach ($dates as $date) {
+        $games = $date->games;
+        foreach($games as $game){
+          $gameState = $game->status->codedGameState;
+
+          if($gameState == '7'){ // 7- game finished
+            $home_team = $game->teams->home->team->name;
+            $away_team = $game->teams->away->team->name;
+            $home_team_points = $game->teams->home->score;
+            $away_team_points = $game->teams->away->score;
+            $game_pk = strval($game->gamePk);
+            $game_id = Game::where('external_game_id', $game_pk)->value('id');
+            //check if result for this game has already been entered
+            if(!Result::where('game_id',$game_id)->first()){
+              //create new result entry in database
+              Result::create(['home_team'      => $home_team,
+                            'away_team'        => $away_team,
+                            'home_team_points' => $home_team_points,
+                            'away_team_points' => $away_team_points,
+                            'game_id'          => $game_id]);
+
+              //update game status in database to ended = true
+              Game::where('external_game_id', $game_pk)->update(['ended'=>true]);
+            }
+          }
+        /*  Game::create(['home_team' => $home,
+                        'away_team' => $away,
+                        'start_time' => date('Y-m-d H:i', strtotime($startDate)),
+                        'ended'      => false,
+                        'external_game_id' => $gameId,
+                        'league_type' => "NHL"]);*/
+        }
+      }
     }
 }
--- app/Models/Result.php
+++ app/Models/Result.php
@@ -8,7 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Result extends Model
 {
     use HasFactory;
-
+    
+    protected $fillable = ['home_team', 'away_team', 'home_team_points', 'away_team_points', 'game_id', 'start_time'];
     public function game()
     {
         return $this->belongsTo('App\Models\Game');
